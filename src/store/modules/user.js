5093bf928d631a0267941024f6f925ebf9b30e24
Dengjianan <dja_542989253@163.com>
2021-12-02 14:30:36 +0800

    'actions的封装'

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,32 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+const state = {
+  token: getToken() // 设置token为共享状态，初始化vuex的时候，就先从缓存中读取
+}
+const mutations = {
+  setToken(state, token) {
+    state.token = token
+    // 同步给缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null
+    removeToken() // 同步给缓存
+  }
+}
+const actions = {
+  async login(context, data) {
+    // 调用api接口
+    const result = await login(data)
+    if (result.data.success) {
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  state,
+  mutations,
+  actions
 }
